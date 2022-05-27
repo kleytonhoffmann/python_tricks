32dd220ca15cfe9c7f66d41b29d417a8a99c1515
Kleyton Hoffmann <kleytonhoffmann@gmail.com>
2022-05-27 11:11:01 -0300

    update path

--- images_to_pdf/images_to_pdf.py
+++ images_to_pdf/images_to_pdf.py
@@ -1,6 +1,6 @@
 import os
 import img2pdf
 
-path = 'C:/Users/Hoffmann/OneDrive/Backupdropbox/UNOESC/python'
+path = 'PATH HERE'
 with open("out.pdf", "wb") as file:
    file.write(img2pdf.convert([i for i in os.listdir(path) if i.endswith(".jpg")]))
